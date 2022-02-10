10c52acb2996c6ac8b1623c14ca992411ee7c14a
Juarez <a1016060@bestbuy.com>
2022-02-10 15:40:32 -0600

    routing from db works

--- app/routes/jokes/$jokeID.jsx
+++ app/routes/jokes/$jokeID.jsx
@@ -1,8 +1,26 @@
+import { useLoaderData } from "remix";
+import { PrismaClient} from '@prisma/client';
 
-const jokeComponent = () => {
-    return(<div>
-        <p>This would catch all items under this route, and pass the url as a parameter to the component loader.</p>
-    </div>);
+const db = new PrismaClient();
+export const loader = async ({params})=> {
+    //load joke from ID that should be in the route params
+    const joke = await db.joke.findUnique({where: { id: params.jokeID }});
+    // if(!joke) {
+    //     throw new Error("Joke not Found");
+    // }
+    return joke;
 }
 
+const jokeComponent = () => {
+
+const loaderData = useLoaderData();
+//loader function to pull data
+console.log(loaderData);
+    return(<div>
+        {loaderData ? <p>Joke Loaded</p> : <p>Joke not loaded</p>}
+        <p>This would catch all items under this route, and pass the url as a parameter to the component loader.</p>
+        <p>{loaderData.content}</p>
+    </div>);
+}
+//export a link function in the route module
 export default jokeComponent;
--- app/routes/jokes/index.jsx
+++ app/routes/jokes/index.jsx
@@ -1,8 +1,21 @@
 
+//you can use a loader function here to pull a random object from the db
+
+// export const loader: LoaderFunction = async () => {
+//     const count = await db.joke.count();
+//     const randomRowNumber = Math.floor(Math.random() * count);
+//     const [randomJoke] = await db.joke.findMany({
+//       take: 1,
+//       skip: randomRowNumber
+//     });
+//     const data: LoaderData = { randomJoke };
+//     return data;
+//   };
+
 function JokesIndexRoute() {
     return(
     <div>
-        <p>Here's a riddle, rendered as a child component.</p>
+        <p>Here's a nested route, all items rendered as a child component.</p>
         <p>blah blah blah</p>
     </div>)
 }
