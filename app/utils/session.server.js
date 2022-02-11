9eba4348a55ddf08bcf7ca8e674c1a8fba588f4e
Juarez <a1016060@bestbuy.com>
2022-02-11 15:57:55 -0600

    setup server util

---
+++ app/utils/session.server.js
@@ -0,0 +1,31 @@
+import {PrismaClient} from '@prisma/client';
+import bcrypt from 'bcryptjs';
+
+const db = new PrismaClient();
+
+export async function login(username, password) {
+    //export a function called login that accepts username and password
+//function should query prisma for a user with username
+const user = await db.user.findUnique({where: {username}});
+//if there is no user, return null
+if (!user) {
+    return null;
+}
+
+//use bcrypt.compare to compare password to users passwordHash
+const isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
+//if passwords don't match return null
+if(isCorrectPassword) { return null };
+//if passwords match, return user
+
+return user;
+}
+
+export async function createUserSession(userID, route) {
+    //create new session
+    //set userID field on the session
+    //redirect to the given route setting the setcookie header
+}
+
+
+
